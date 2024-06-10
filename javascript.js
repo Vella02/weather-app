83d2d8d4b1571ef6e0a7686689bb49da2aa4c2b0
Michael Vella <michael12vella@gmail.com>
2024-06-10 15:00:23 -0400

    learning git

---
+++ javascript.js
@@ -0,0 +1,14 @@
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=1f1c5b1914bd4d49867152530240606&q=paris",
+      { mod: "cors" }
+    );
+    const weather = await response.json();
+    console.log(weather.current.condition.text);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeatherData();
